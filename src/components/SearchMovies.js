1d04b83f2b10488e1caf7915dbfd29c0c50ea7f6
josuecedeno <josuecedeno@yahoo.com>
2021-02-20 12:51:53 -0600

    Initial commit

---
+++ src/components/SearchMovies.js
@@ -0,0 +1,51 @@
+import React, { useState, Fragment } from 'react';
+import MovieCard from './MovieCard';
+
+export default function SearchMovies() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const API_KEY = '052ce0ce36d817ba76dcc79da15402e5';
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(URL);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Fragment>
+      <form className='form' onSubmit={searchMovies}>
+        <label htmlFor='query' className='label'>
+          Movie Name
+        </label>
+        <input
+          type='text'
+          className='input'
+          name='query'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder='Type movie title here'
+        ></input>
+        <button className='button' type='submit'>
+          Search
+        </button>
+      </form>
+
+      <div className='card-list'>
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+      </div>
+    </Fragment>
+  );
+}
